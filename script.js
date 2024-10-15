bd276f7a856099668ad532c98ccee5c1737e7163
MatheusAndradeeee <115434474+MatheusAndradeeee@users.noreply.github.com>
2024-10-14 20:53:04 -0400

    Adiciona calculadora de soma simples

---
+++ script.js
@@ -0,0 +1,8 @@
+function soma(){
+    var numero1 = document.getElementById("numero1").value;
+    var numero2 = document.getElementById("numero2").value;
+
+    var resultado = Number(numero1) + Number(numero2);
+
+    alert("Resultado = " + resultado)
+}
